7271c0f34b8e3ab03f9f7bd0f1ad5d54a736f7c1
Hawstein <tomhawstein@gmail.com>
2012-10-24 22:35:54 +0800

    use template: kylexlau.github.com and write the first post: ibus-wubi

---
+++ _includes/google-analytics.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-4770949-2']);
+  _gaq.push(['_setDomainName', 'xlau.org']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
