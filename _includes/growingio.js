769d83419d1c8554abb331d73851269c529b28bb
Hawstein <tomhawstein@gmail.com>
2016-04-05 21:23:39 +0800

    add growingio js sdk

---
+++ _includes/growingio.js
@@ -0,0 +1,15 @@
+<script type='text/javascript'>
+      var _vds = _vds || [];
+      window._vds = _vds;
+      (function(){
+        _vds.push(['setAccountId', '8e8614c1393e3f65']);
+        (function() {
+          var vds = document.createElement('script');
+          vds.type='text/javascript';
+          vds.async = true;
+          vds.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'dn-growing.qbox.me/vds.js';
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(vds, s);
+        })();
+      })();
+  </script>
